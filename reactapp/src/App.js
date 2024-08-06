967751986f929e447640614e5b4c4594509d5383
benha04 <133995672+benha04@users.noreply.github.com>
2024-08-06 17:45:28 -0400

    added auth

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import Navbar from './components/navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Authenticator loginMechanism={['email']}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Authenticator>
   );
 }
 
