55a85d116e317c5b2be9435f14f24e148b3b768e
benha04 <133995672+benha04@users.noreply.github.com>
2024-08-07 16:07:38 -0400

    [Prep-224] Create React Project and Auth (#6)
    * added react project and basic navbar
    
    * added auth
    
    * fixed auth
    
    * updated projet to reflect jack's comments

---
+++ reactapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "htmlsnippets0d466330": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ reactapp/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+
+
+Amplify.configure(awsExports);
+
+function App() {
+  return (
+    <Authenticator loginMechanism={['email']}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Authenticator>
+  );
+}
+
+export default App;
