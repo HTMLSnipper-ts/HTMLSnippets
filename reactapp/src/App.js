0004c2760a4c645f6c34fbd8bffd952aa1b996cb
benha04 <133995672+benha04@users.noreply.github.com>
2024-08-07 01:38:37 -0400

    fixed auth

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import { Amplify } from 'aws-amplify';
-
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
 import awsExports from './aws-exports';
+
+
 Amplify.configure(awsExports);
 
 function App() {
