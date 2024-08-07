9523cf9175345e611f1ca142a2670228bdf0ec8b
benha04 <133995672+benha04@users.noreply.github.com>
2024-08-07 13:47:11 -0400

    updated projet to reflect jack's comments

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import { Amplify } from 'aws-amplify';
