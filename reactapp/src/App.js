e6790e83efdfe95547c243421b5f9d5d60d8c4a0
chrislamvt <147569268+chrislamvt@users.noreply.github.com>
2024-08-15 13:35:53 -0400

    [Prep-225] Create Code Editor (#7)
    * Code editor, not fully working yet
    
    * Fixed bugs with cursor position and deletion.

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
+import LiveCodeEditor from './pages/editor/liveCodeEditor';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
+import ReactDOM from 'react-dom';
 
 
 Amplify.configure(awsExports);
@@ -17,6 +19,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/code-editor" element={<LiveCodeEditor />} />
         </Routes>
       </Router>
     </Authenticator>
