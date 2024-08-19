4309593d327046f0ef69965962a3dce82791a985
benha04 <133995672+benha04@users.noreply.github.com>
2024-08-19 11:49:20 -0400

    [PREP-242 and signout] (#8)
    * added react project and basic navbar
    
    * added auth
    
    * fixed auth
    
    * updated projet to reflect jack's comments
    
    * added signout functionality and favorites tab
    
    * Update App.js
    
    * Update team-provider-info.json
    
    * navbar now displays username
    
    * Update Navbar.js
    
    * Update Navbar.js
    
    * updated app.js

--- reactapp/src/components/navbar/Navbar.js
+++ reactapp/src/components/navbar/Navbar.js
@@ -1,42 +1,54 @@
 import React from 'react';
-import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink
-} from './NavbarElements';
+import { signOut } from 'aws-amplify/auth';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { Nav, NavLink, Bars, NavMenu, NavBtn } from './NavbarElements';
+
+
+async function handleSignOut() {
+  try {
+    await signOut();
+    window.location.href = '/signin';
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
+}
 
 const Navbar = () => {
+  const { user } = useAuthenticator((context) => [context.user]);
+
   return (
-    <>
-      <Nav>
-        <NavLink to='/'>
-          Logo
-        </NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to='/code-editor' activeStyle>
+    <Nav>
+      <NavLink to='/'>
+        Logo
+      </NavLink>
+      <Bars />
+      <NavMenu>
+      <NavLink to='/code-editor' activeStyle>
             HTML
           </NavLink>
-          <NavLink to='/services' activeStyle>
-            CSS
-          </NavLink>
-          <NavLink to='/contact-us' activeStyle>
-            JS
-          </NavLink>
-          <NavLink to='/sign-up' activeStyle>
-            Sign Up
-          </NavLink>
-          {/* Second Nav */}
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-        </NavBtn>
-      </Nav>
-    </>
+        <NavLink to='/services'>
+          CSS
+        </NavLink>
+        <NavLink to='/contact-us'>
+          JS
+        </NavLink>
+        <NavLink to='/sign-up'>
+          Sign Up
+        </NavLink>
+        {user && (
+          <>
+            <NavLink to='/favorites'>
+              Favorites
+            </NavLink>
+            <span style={{ marginLeft: '20px', color: 'white' }}>
+              Hello, {user.username}!
+            </span>          </>
+        )}
+      </NavMenu>
+      <NavBtn>
+        <button onClick={handleSignOut}>Sign Out</button>
+      </NavBtn>
+    </Nav>
   );
 };
 
